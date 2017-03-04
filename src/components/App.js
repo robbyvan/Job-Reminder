0390dab3cda74f83eed79b4617e383e1a4cd3362
robbyvan <fzpinus@gmail.com>
2017-03-04 02:16:10 -0800

    init

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <h1>This is my app</h1>
+      </div>
+    );
+  }
+
+}
